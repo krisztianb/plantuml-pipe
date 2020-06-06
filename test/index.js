8285094c6d37cf27b1cd64cdcbac4635204ccab7
Krisztian Balla <k.balla@gmx.at>
2020-06-06 18:47:36 +0200

    Add test script

--- test/index.js
+++ test/index.js
@@ -4,12 +4,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 
 var fs = require("fs");
 var plantuml_piper_1 = require("../dist/plantuml_pipe");
-var puml = new plantuml_piper_1.PlantUmlPipe();
-var rstream = fs.createReadStream("./hello2.puml");
-var fileCounter = 0;
 
-rstream.pipe(puml.in);
-puml.out.on("data", function (chunk) {
-    fs.writeFileSync("./" + fileCounter + ".svg", chunk);
-    ++fileCounter;
-});
+testSvg();
+testPng();
+
+// Create SVG images
+function testSvg() {
+    var puml = new plantuml_piper_1.PlantUmlPipe();
+    var rstream = fs.createReadStream("./hello2.puml");
+    var fileCounter = 0;
+
+    rstream.pipe(puml.in);
+    puml.out.on("data", function (chunk) {
+        fs.writeFileSync("./" + fileCounter + ".svg", chunk);
+        ++fileCounter;
+    });
+}
+
+// Crearte PNG images using a different delimiter
+function testPng() {
+    var puml = new plantuml_piper_1.PlantUmlPipe({ outputFormat: "png", delimiter: "-----SNIP-----" });
+    var rstream = fs.createReadStream("./hello2.puml");
+    var fileCounter = 0;
+
+    rstream.pipe(puml.in);
+    puml.out.on("data", function (chunk) {
+        fs.writeFileSync("./" + fileCounter + ".png", chunk);
+        ++fileCounter;
+    });
+}
