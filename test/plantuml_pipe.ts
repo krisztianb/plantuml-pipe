cecde60af36c4fedb54e2e1cbc519549b069a34a
Krisztian Balla <k.balla@gmx.at>
2021-11-24 21:51:12 +0100

    Setup mocha for testing

---
+++ test/plantuml_pipe.ts
@@ -0,0 +1,14 @@
+import assert from "assert";
+import { PlantUmlPipe } from "../src/plantuml_pipe";
+
+describe("plantuml_pipe", () => {
+    it("should create SVG images", () => {});
+
+    it("should create PNG images using a different delimiter", () => {});
+
+    it("should use the pixel cut off value", () => {});
+
+    it("should not create error images", () => {});
+
+    it("should fail with invalid JAR path", () => {});
+});
