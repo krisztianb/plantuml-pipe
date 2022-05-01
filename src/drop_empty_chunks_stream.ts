1248ebb962210326c2249e24e40d2049366ccede
Krisztian Balla <k.balla@gmx.at>
2022-05-01 14:35:29 +0200

    Update dependencies

--- src/drop_empty_chunks_stream.ts
+++ src/drop_empty_chunks_stream.ts
@@ -1,4 +1,5 @@
 import { Transform, TransformCallback } from "stream";
+import { DeepReadonly } from "ts-essentials";
 
 /**
  * A transformation stream that doesn't forward empty chunks.
@@ -12,7 +13,11 @@ export class DropEmptyChunksStream extends Transform {
      * @param callback A callback function (optionally with an error argument and data) to be called
      *                 after the supplied chunk has been processed.
      */
-    public override _transform(chunk: Buffer | string, _encoding: string, callback: TransformCallback): void {
+    public override _transform(
+        chunk: DeepReadonly<Buffer> | string,
+        _encoding: string,
+        callback: TransformCallback,
+    ): void {
         if (chunk.length > 0) {
             this.push(chunk);
         }
